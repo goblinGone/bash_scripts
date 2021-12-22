42149d1b619ad89c504223fc71adaca10bf111bd
James A Ware <jamesaware@rocketmail.com>
2021-12-22 18:41:40 +0000

    working again

--- backup.sh
+++ backup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 today=$(date)
-mkdir home/pi/backups
+mkdir backups
 echo "we will back up some stuff now"
 echo "Please name the working directory"
 read working_dir
 echo "please list the files to be backed-up"
 read todo_list
-echo "what do you wish the file to be called?"
-read file_name
 for i in $todo_list
-do
-tar --create --gzip --verbose ~/$working_dir/$i --file=$file_name$(date -I).tar.gz -C /home/pi/backups
-echo "ok, file $i has been added"
+  do
+  tar -cgv ../pi/backups/$i$(date -I).tar.gz ~/$working_dir/$i
+  echo "ok, file $i has been added"
+  done
+echo "Back up complete, have a nice day"
