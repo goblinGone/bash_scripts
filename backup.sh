49839765a3ea72a0cf01347dd6989056dbd1689f
James A. Ware <jamesaware@rocketmail.com>
2022-01-22 16:04:23 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
+
+echo " "
+echo "***************************************"
+echo "** Jamsbot's File Backup'inator"
+echo "***************************************"
+echo " "
+
 today=$(date)
-mkdir backups
-echo "we will back up some stuff now"
-echo "Please name the working directory"
+
+echo "** we will back up some stuff now"
+echo " "
+echo "** Please name the directorypath from user ie /home/user/SOME/OTHER/DIR"
 read working_dir
-echo "please list the files to be backed-up"
+echo "** please type the file derectory to be backed-up"
 read todo_list
+
+#todo_list=todo_list
+#todo_list=$(ls $todo)
+#echo "$todo_list"
+
+if [ ! -d "$backups"]; then
+    echo "dir present"
+else
+    mkdir backups
+    echo "** creating a backup directory"
+fi
+
 for i in $todo_list
-do
-tar -cgv -f ../pi/backups/$i$(date -I).tar.gz ~/$working_dir/$i
-  echo "ok, file $i has been added"
-done
-echo "Back up complete, have a nice day"
+  do
+    tar -cf ~/$working_dir/backups/$i$(date -I).tar $i
+      echo " ... ok, file $i has been added"
+  done
+
+echo " "
+echo "***************************************"
+echo "** Back up Complete Have a nice day"
+echo "***************************************"
+echo " "
