f654e7cdf60091a0d6d14ed2afbd0a87985a2d37
James A. Ware <jamesaware@rocketmail.com>
2022-03-30 16:43:05 +0100

    progressBar

---
+++ progressBar.sh
@@ -0,0 +1,11 @@
+for ((k = 0; k <= 10 ; k++))
+do
+    echo -n "[ "
+    for ((i = 0 ; i <= k; i++)); do echo -n "###"; done
+    for ((j = i ; j <= 10 ; j++)); do echo -n "   "; done
+    v=$((k * 10))
+    echo -n " ] "
+    echo -n "$v %" $'\r'
+    sleep 0.2
+done
+echo
