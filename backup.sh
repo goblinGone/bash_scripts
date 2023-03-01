1c778f82cbc8d3cf786a21d1d68d8129724258c9
Status <89606931+goblinGone@users.noreply.github.com>
2023-03-01 15:34:58 +0000

    updates
    updated relative file paths and also amended the zip type

--- backup.sh
+++ backup.sh
@@ -13,34 +13,15 @@ echo "** Jamsbot's File Backup'inator"
 echo "***************************************"
 echo " "
 
-today=$(date)
+now=`date +"%Y-%m-%d"`
 
-echo "** we will back up some stuff now"
-echo " "
-echo "** Please name the directorypath from user ie /home/user/SOME/OTHER/DIR"
-read working_dir
-echo "** please type the file derectory to be backed-up"
-read todo_list
+tar -cvjf system_backup_${now}.tar.gz ~/CAD/parts ~/CAD/projects ~/CodeBase ~/Documents ~/Library ~/Project_archive ~/Templates
 
-#todo_list=todo_list
-#todo_list=$(ls $todo)
-#echo "$todo_list"
-
-if [ ! -d "$backups"]; then
-    echo "dir present"
-else
-    mkdir backups
-    echo "** creating a backup directory"
-fi
-
-for i in $todo_list
-  do
-    tar -cf ~/$working_dir/backups/$i$(date -I).tar $i
-      echo " ... ok, file $i has been added"
-  done
+echo "Selected files have been archived!"
 
 echo " "
 echo "***************************************"
 echo "** Back up Complete Have a nice day"
 echo "***************************************"
 echo " "
+exit
