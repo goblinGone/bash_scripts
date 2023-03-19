035ddf1a65640c2a72c46a1d59ea40f95af17c4a
Status <jamesaware@rocketmail.com>
2023-03-19 20:42:23 +0000

    Delete scrapper.sh

--- scrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/
-curl -s -A "reddit scraper" "https://www.reddit.com/r/VillagePorn.json?limit=100" | \
-jq '.' | \
-grep "url_overridden_by_dest" | \
-sed 's/\"url_overridden_by_dest\"\: //;s/ * //g;s/\"//g'
