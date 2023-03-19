9e3e4daa323d441d1129bfe7b0f96e78f236dbc7
Status <jamesaware@rocketmail.com>
2023-03-19 13:40:51 +0000

    folder structure change

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ echo " "
 
 now=`date +"%Y-%m-%d"`
 
-tar -cvjf system_backup_${now}.tar.gz ~/CAD/parts ~/CAD/projects ~/CodeBase ~/Documents ~/Library ~/Project_archive ~/Templates
+tar -cvjf system_backup_${now}.tar.gz ~/BIN_REG ~/CAD/parts ~/CAD/projects ~/CodeBase/__non_cloud_sync ~/CodeBase/Arduino ~/Documents ~/Downloads ~/Project_archive 
 
 echo "Selected files have been archived!"
 
