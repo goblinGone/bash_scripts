0602218e96361b7fe47267a3d224f5879afe479b
Status <jamsaware@gmail.com>
2023-04-12 23:03:51 +0100

    Update mkprj.pl
    updated

--- mkprj.pl
+++ mkprj.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
-#   make project script, 
-#   be sure to check the file paths is erreos are thrown
+#   make project script,
+#   be sure to check the file paths if errors are thrown
 #
 
 use warnings;
@@ -19,9 +19,6 @@ print "*********************************************\n".
 my $name = <STDIN>;
 chomp($name);
 $name=~s/ /-/g;
-print "** please offer some tags seperated by a space: ";
-my $tags = <STDIN>;
-chomp($tags);
 print "* please offer priority; ";
 my $priority = <STDIN>;
 chomp($priority);
@@ -35,39 +32,18 @@ print $fh "---\n".
 "alias:\n".
 "cssclass:\n".
 "lastReviewed:\n".
-"tags: $tags\n".
 "isProject: true\n".
 "note:\n".
 "priority: $priority\n".
 "projCompleted:\n".
-"projectMain: $name\n".
 "projectType:\n".
 "status: planning\n".
 "date: $date\n".
-"---\n\n".
+"---\n".
+"**Related:**\n".
+"-\n".
 "# $name\n\n\n\n\n".
-"```dataview\n".
-"task\n".
-"from \"TOOLS\"\n".
-"where !completed & projectMain = \"$name\" & type = \"taskList\"\n".
-"```\n\n\n".
-"[[$name-taskList]]\n".
 "[$name folder](file:///home/goblin/Project_archive/$name)\n".
 "[This project is licenced using the Gnu GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html)";
-
-
 close $fh;
 print "** done project page\n";
-my $taskfile = "/home/goblin/Zettel/GoblinKnows/TOOLS/projectFile_taskLists/$name-taskList.md";
-open(my $tf, '>', $taskfile) or die "could not open file '$taskfile' $!";
-print $tf "---\n".
-"cssstyle:\n".
-"isProject: true\n".
-"projectMain: $name\n".
-"type: taskList\n".
-"date: $date\n".
-"---\n\n\n".
-"# $name-taskList\n".
-"[[$name|Link to main project]]";
-close $tf;
-print "** done taskList page\n";
