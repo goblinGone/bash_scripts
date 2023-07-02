6769993abe244f18c247402e4859555132dfce60
Status <jamsaware@gmail.com>
2023-07-02 23:03:33 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,24 +1,41 @@
-#
-#   simple backup script
-#   runs through the listed folders adding to a compressed file
-#
-
 #!/bin/bash
-echo " "
-echo "***************************************"
-echo "** Jamsbot's File Backup'inator"
-echo "***************************************"
-echo " "
+
+txtblu='\e[0;34m'
+txtcyn='\e[0;36m'
+txtylw='\e[0;33m'
+txtwht='\e[0;37m'
 
 now=`date +"%Y-%m-%d"`
+dir=$(pwd)
+result=${PWD##*/}
+mkdir ~/backup_$now
+clear
 
-tar -cvjf system_backup_${now}.tar.gz ~/BIN_REG ~/CAD/parts ~/CAD/projects ~/CodeBase/__non_cloud_sync ~/CodeBase/Arduino ~/Documents ~/Downloads ~/Project_archive 
+for i in $dir/*  
+do
+    if [ -d "$i" ] 
+    then
+        echo -e ${txtblu}$(basename "$i")${txtwht}
 
-echo "Selected files have been archived!"
+        temp=$(basename "$i")
+        mkdir ~/backup_$now/$temp
 
-echo " "
-echo "***************************************"
-echo "** Back up Complete Have a nice day"
-echo "***************************************"
-echo " "
-exit
+        cd $i
+
+        dir2=$(pwd)
+        
+        for j in $dir2/* ; do
+            if [ -d "$j" ]; then
+                echo -e ${txtcyn}$(basename "$j")${txtwht}
+            elif [ -f "$j" ] 
+            then 
+                echo -e ${txtylw}$(basename "$j")${txtwht} 
+            fi
+        done
+    elif [ -f "$i" ] 
+    then
+        echo -e ${txtylw}$(basename "$i")${txtwht}   
+    fi
+done
+
+
