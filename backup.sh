adc062cf5cc965c0b98e80ec25c64a653dc989b3
goblinGone <jamsaware@gmail.com>
2023-07-03 21:45:50 +0100

    backup file is bork

--- backup.sh
+++ backup.sh
@@ -15,26 +15,39 @@ for i in $dir/*
 do
     if [ -d "$i" ] 
     then
-        echo -e ${txtblu}$(basename "$i")${txtwht}
-
-        temp=$(basename "$i")
-        mkdir ~/backup_$now/$temp
-
         cd $i
+        echo -e ${txtblu}$(basename "$i")${txtwht}
+        echo $i
+        mkdir ~/backup_$now/$i
+
+        #tar -cvf $i.tar ~/backup_$now/$i
 
         dir2=$(pwd)
-        
-        for j in $dir2/* ; do
-            if [ -d "$j" ]; then
-                echo -e ${txtcyn}$(basename "$j")${txtwht}
-            elif [ -f "$j" ] 
-            then 
-                echo -e ${txtylw}$(basename "$j")${txtwht} 
-            fi
-        done
-    elif [ -f "$i" ] 
+        cd ..
+        # for j in $dir2/* 
+        # do
+            
+        #     if [ -d "$j" ]
+        #     then
+        #         cd $j
+        #         echo -e ${txtcyn}$(basename "$j")${txtwht}
+        #         tar -cvf $j.tar $j 
+        #         cd ..
+
+        #     elif [ -f "$j" ] 
+        #     then 
+        #         cd $j
+        #         echo -e ${txtylw}$(basename "$j")${txtwht} 
+        #         tar -cvf $jfiles.tar $j 
+        #         cd ..
+        #     fi
+        # done
+    elif [ -f "$i"  ] 
     then
-        echo -e ${txtylw}$(basename "$i")${txtwht}   
+
+        echo -e ${txtylw}$(basename "$i")${txtwht}
+        #tar -cvfP $temp.tar $j   
+
     fi
 done
 
